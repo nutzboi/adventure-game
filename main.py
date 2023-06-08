92b91dab2e5caf9868dc82424ef49cc992b49925
nutzboi <nutzboi@bingchilling.com>
2023-06-08 11:47:37 +0300

    Finished. Untested.

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import random
 
 player_name = "player"
 
-inventory_stick = 0
-inventory_empty = 1
+stat = {
+    "inventory_stick": 0,
+    "inventory_empty": 1,
+    "monster_alive": 1,
+    "monster_beaten": 0,
+    "candy_eaten": 0,
+    "candy_cave": 1 
+}
 
 situation = ""
 score = 0
@@ -24,9 +30,9 @@ situations = {
                   "There seems to be a fire lit inside the left cave.",
                   "The right cave is so dark you can't see anything.",
                   "Which cave will you enter?"]],
-        "prompt": [["Left Cave.", "Right Cave.", "Right Cave."]],
-        "results": [["cave1", "cave2", "cave2_empty"]],
-        "availability": [["", "cave_stick","inventory_stick"]]
+        "prompt": [["Left Cave.", "Left Cave." "Right Cave.", "Right Cave."]],
+        "results": [["cave1", "cave1_walk" "cave2", "cave2_empty"]],
+        "availability": [["monster_alive", "monster_beaten", "cave_stick","inventory_stick"]]
     },
     "cave1":
     {
@@ -44,9 +50,12 @@ situations = {
         "func": [""],
         "desc": [["You step inside the cave. You're scared.",
                   "You step on an object. What to do?"]],
-        "prompt": [["Pick it up.", "Continue walking.", "Go back."]],
-        "results": [["cave2_obj", "cave2_walk", "initial"]],
-        "availability": [["", "", ""]]
+        "prompt": [["Pick it up.", "Continue walking.",
+                    "Continue walking.", "Go back."]],
+        "results": [["cave2_obj", "cave2_walk",
+                     "cave2_walk_empty", "initial"]],
+        "availability": [["", "candy_cave",
+                          "candy_eaten", ""]]
     },
     "cave2_empty":
     {
@@ -54,9 +63,9 @@ situations = {
         "func": [""],
         "desc": [["You're inside the cave.",
                   "It's dark. What to do?"]],
-        "prompt": [["Continue walking.", "Go back."]],
-        "results": [["cave2_walk", "initial"]],
-        "availability": [["", ""]]
+        "prompt": [["Continue walking.", "Continue walking.", "Go back."]],
+        "results": [["cave2_walk", "cave2_walk_empty", "initial"]],
+        "availability": [["candy_cave", "candy_eaten", ""]]
     },
     "cave2_obj":
     {
@@ -64,9 +73,30 @@ situations = {
         "func": ["pickup_stick","score+1"],
         "desc": [["You now have a stick.",
                   "Fantastick! (+1 point ^ω^)"]],
-        "prompt": [["Continue"]],
+        "prompt": [["Continue."]],
         "results": [["cave2_empty"]],
         "availability": [[""]]
+    },
+    "cave2_walk":
+    {
+        "cases": 1,
+        "func": ["score+1", "pickup_candy"],
+        "desc": [["You walk further inside the cave.",
+                  "You find some candy to take with you.",
+                  "Nice. (+1 point ^ω^)"]],
+        "prompt": [["Continue."]],
+        "results": [["cave2_empty"]],
+        "availability": [[""]]
+    },
+    "cave2_walk_empty":
+    {
+        "cases": 1,
+        "func": ["score+1"],
+        "desc": [["You walk further inside the cave.",
+                  "There's nothing here."]],
+        "prompt": [["Go back.","Go back."]],
+        "results": [["cave2","cave2_empty"]],
+        "availability": [["inventory_empty","inventory_stick"]]
     },
     "cave1_hello":
     {
@@ -85,42 +115,78 @@ situations = {
                     ["cave1_inside", "initial"]],
         "availability": [["", "", "inventory_stick"], ["", ""]]
     },
+    "cave1_walk":
+    {
+        "cases": 1,
+        "func": ["show_score"],
+        "desc": [["You walk deep inside the left cave.", "You find a map.",
+                  "Now you can find your way home!", "Yᴏᴜ Wɪɴ."]],
+        "prompt": [["Main menu."]],
+        "results": [["menu"]],
+        "availability": [[""]]
+    },
     "escape_monster":
     {
         "cases": 1,
         "func": [""],
-        "desc": ["You are now outside.",
-                 "The monster is still after you. What to do?"],
-        "prompt": ["Go into the other cave.",
+        "desc": [["You are now outside.",
+                 "The monster is still after you. What to do?"]],
+        "prompt": [["Go into the other cave.",
                    "Fight it with your fist.",
-                   "Beat it with your stick.", "Give up."],
-        "results": ["escape_cave2", "fight_monster", "beat_monster", "give_up"],
-        "availability": ["", "", "inventory_stick", ""]
+                   "Beat it with your stick.", "Give up."]],
+        "results": [["escape_cave2", "fight_monster", "beat_monster", "give_up"]],
+        "availability": [["", "", "inventory_stick", ""]]
     },
     "fight_monster":
     {
         "cases": 1,
-        "func": [""],
+        "func": ["score-1"],
         "desc": [["You try to fight the monster with your fist.",
-        "It's too strong for you. You are hurt (-1 point T_T)."]],
-        "prompt": [["Beat it with your stick.","Continue"]],
-        "results": [["beat_monster","lose"]],
-        "availability": [["inventory_stick","inventory_empty"]]
+        "It's too strong for you.","You are hurt (-1 point T_T)."]],
+        "prompt": [["Beat it with your stick.","Give up."]],
+        "results": [["beat_monster","give_up"]],
+        "availability": [["inventory_stick",""]]
     },
     "beat_monster":
     {
         "cases": 1,
-        "func": ["use_stick"],
+        "func": ["score+1"],
         "desc": [["You beat the monster with your stick.",
-        "It falls to the ground. You win."]],
-        "prompt": [["Continue"]],
-        "results": [["win"]],
+        "It falls to the ground.","(+2 points ^ω^)"]],
+        "prompt": [["Continue."]],
+        "results": [["relax"]],
         "availability": [[""]]
     },
     "escape_cave2":
     {
+        "cases": 2,
+        "func": [],
+        "desc": [["Dead End. The monster eats you.", "Gᴀᴍᴇ Oᴠᴇʀ."],
+                 ["The monster loses its sight of you.",
+                  "It wanders into the forest.",
+                  "You escaped the monster. (+1 points ^ω^)"]],
+        "prompt": [["Main menu."], ["Continue."]],
+        "results": [["menu"],["relax"]],
+        "availability": [[""],[""]]
+    },
+    "relax":
+    {
         "cases": 1,
-        "func": [""]
+        "func": ["score+1"],
+        "desc": [["You sit outside for a while to take your breath.",
+                 "You're proud of yourself and ready to get up agian. (°v^)",]],
+        "prompt": [["Continue."]],
+        "results": [["initial"]],
+        "availability": [[""]]
+    },
+    "give_up":
+    {
+        "cases": 1,
+        "func": ["show_score"],
+        "desc": [["You are hopeless.", "You let the monster eat you.", "Gᴀᴍᴇ Oᴠᴇʀ."]],
+        "prompt": [["Main menu."]],
+        "results": ["menu"],
+        "availability": [""]
     }
 }
 
@@ -133,18 +199,27 @@ def print_pause(s, t=1):
     time.sleep(t)
 
 
-def prompt(options):
+def prompt(options,available):
     '''
     Input function to keep prompting users with prompts "options"
     until a valid option number is chosen.
     '''
+    option_numbers = []
+    last_number = 1
+    for i in available:
+        if(stat[i]):
+            option_numbers.append(last_number)
+            last_number+=1
+        else:
+            option_numbers.append(-1)
     while True:
         for i in range(len(options)):
-            print("("+str(i+1)+") "+options[i])
+            if i == -1:
+                continue
+            print("("+str(option_numbers[i])+") "+options[i])
         choice = input("Input choice number: ")
-        choice-=1
-        if choice < len(options):
-            return choice
+        if choice < last_number:
+            return option_numbers.index(choice)
         else:
             print("Please choose a valid option")
 
@@ -157,24 +232,36 @@ def init():
     print_pause("Welcome to the game.", 3)
     player_name = str(input("Please enter your name: "))
 
-def func(s):
-    if(s == "inventory_stick"):
-        return stick
-    if(s == "cave_stick"):
-        return not stick
-    if(s[:5] == "score"):
-        if(s[5] == "+"):
-            score+=int(s[6:])
-        else:
-            score-=int(s[6:])
+def func(f):
+    '''
+    Execute functions demanded by the game situation. 
+    '''
+    for s in f:
+        if(s == "show_score"):
+            print("Your score is: " + str(score) + "points.")
+        if(s == "pickup_stick"):
+            stat["inventory_empty"] = 0
+            stat["inventory_stick"] = 1
+        if(s == "pickup_candy"):
+            stat["candy_eaten"] = 1
+            stat["candy_cave"] = 0
+        if(s[:5] == "score"):
+            if(s[5] == "+"):
+                score+=int(s[6:])
+            else:
+                score-=int(s[6:])
     
 def gameplay():
+    '''
+    Process Gameplay.
+    '''
     situation = "initial"
-    while situation != "game_over":
+    while situation != "menu":
+        func(situations[situation]["func"])
         situation_case = random.randrange(0,situations["situation"]["cases"])
         for i in situations[situation]["desc"][situation_case]:
             print_pause(i,1 if len(i) < 35 else 2)
-        choice = prompt(situations[situation]["prompt"][situation_case])
+        choice = prompt(situations[situation]["prompt"][situation_case],situations[situation]["availability"][situation_case])
         situation = situations["situation"]["results"][choice]
 
 
@@ -196,4 +283,5 @@ def menu():
 
 if player_name == "player":
     init()
-menu()
+while(True):
+    menu()
